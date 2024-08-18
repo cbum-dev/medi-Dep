3c479a278953e87ad4821b11db62c5df67103ac1
cbum-dev <priyanshukumar2002234@gmail.com>
2024-08-18 12:35:49 +0530

    added new db

--- Medical/settings.py
+++ Medical/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
+DATABASE_URL = "postgresql://postgres:sOlopnapOCSvZFmmxlUyiglfsGkQWZnG@junction.proxy.rlwy.net:36746/railway"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,17 +81,20 @@ WSGI_APPLICATION = 'Medical.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'railway',
-       'USER': 'postgres',
-       'PASSWORD': '-342daAA2bB-CgbDDCBCcGA5eacc4ADC',
-       'HOST': 'monorail.proxy.rlwy.net',
-       'PORT': '31564',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'railway',
+#        'USER': 'postgres',
+#        'PASSWORD': '-342daAA2bB-CgbDDCBCcGA5eacc4ADC',
+#        'HOST': 'monorail.proxy.rlwy.net',
+#        'PORT': '31564',
+#     }
+# }
 
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL,conn_max_age=1800)
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
